d21e2894ced2731cf40b1ac5a27ddde740c4710c
andresdrew02 <alumno.254958@ies-azarquiel.es>
2025-04-07 17:36:20 +0100

    Update Login Page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,20 @@
 import LoginForm from "@/components/auth/LoginForm";
+import SpinnerFullScreen from "@/components/ui/spinnerFullScreen";
+import { UseUserStore } from "@/store/userStore";
 
 export default function LoginPage() {
+  const user = UseUserStore((state) => state.user);
+  if (user) {
+    window.location.href = "/";
+  }
+
   return (
     <div className="flex flex-col h-screen items-center justify-center w-full">
+      <h1 className="text-2xl font-bold text-slate-800 mb-4">Login Page - YT Converter Client</h1>
       <div className="max-w-lg min-w-lg">
         <LoginForm />
       </div>
+    { user && <SpinnerFullScreen/>}
     </div>
   )
 }
