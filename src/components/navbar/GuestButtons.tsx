932cc210bf644b72e492d2268c518ed4eaae55d3
andresdrew02 <alumno.254958@ies-azarquiel.es>
2025-04-09 18:45:54 +0100

    Update GuestButtons.tsx

--- src/components/navbar/GuestButtons.tsx
+++ src/components/navbar/GuestButtons.tsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 import { Button } from "../ui/button";
 
 export default function GuestButtons () {
     return (
         <div className="flex gap-2">
-            <Button variant="outline">Admin button 1</Button>
-            <Button variant="outline">Admin button 2</Button>
-            <Button variant="outline">Admin button 3</Button>
+            <Link to='/convert'><Button variant="outline" className="cursor-pointer">Convert a video</Button></Link>
+            <Button variant="outline" className="cursor-pointer">Recent queries</Button>
+            <Button variant="outline" className="cursor-pointer">Converted history</Button>
         </div>
     )
 }
