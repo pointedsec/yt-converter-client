639e545d5d8229b0011c70c783afb12b2d370ecc
andresdrew02 <alumno.254958@ies-azarquiel.es>
2025-04-11 07:42:48 +0100

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,20 +1,40 @@
 import LoginForm from "@/components/auth/LoginForm";
+import ChangeThemeButton from "@/components/ui/change-theme-button";
 import SpinnerFullScreen from "@/components/ui/spinnerFullScreen";
+import { getStorageTheme } from "@/lib/storage";
 import { UseUserStore } from "@/store/userStore";
+import { useState } from "react";
 
 export default function LoginPage() {
   const user = UseUserStore((state) => state.user);
+  const [theme, setTheme] = useState<"light" | "dark">(getStorageTheme() || "light");
   if (user) {
     window.location.href = "/";
   }
 
   return (
-    <div className="flex flex-col h-screen items-center justify-center w-full">
-      <h1 className="text-2xl font-bold text-slate-800 mb-4">Login Page - YT Converter Client</h1>
-      <div className="max-w-lg lg:min-w-lg">
-        <LoginForm />
+    <>
+      <div className="absolute flex w-full justify-center m-4">
+        <ChangeThemeButton callback={setTheme} />
       </div>
-    { user && <SpinnerFullScreen/>}
-    </div>
+      <div className="flex flex-col h-screen items-center justify-center w-full">
+        <div className="relative">
+          <img 
+            src="/logo.png" 
+            alt="MediaFlow Logo" 
+            className={`max-w-[200px] max-h-[100px] md:max-w-[300px] md:max-h-[150px] transition-opacity duration-200 ${theme === 'light' ? 'opacity-100' : 'opacity-0 absolute'}`} 
+          />
+          <img 
+            src="/logo_dark.png" 
+            alt="MediaFlow Logo" 
+            className={`max-w-[200px] max-h-[100px] md:max-w-[300px] md:max-h-[150px] transition-opacity duration-200 ${theme === 'dark' ? 'opacity-100' : 'opacity-0 absolute'}`} 
+          />
+        </div>
+        <div className="max-w-lg lg:min-w-lg">
+          <LoginForm />
+        </div>
+        {user && <SpinnerFullScreen />}
+      </div>
+    </>
   )
 }
